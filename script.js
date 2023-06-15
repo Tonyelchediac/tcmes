888d4d83f729149b6d85bdf9774437c39fdfec8a
Tonyelchediac <130777962+Tonyelchediac@users.noreply.github.com>
2023-06-15 16:58:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const menuItems = menu.querySelectorAll('li');
+
+let isMenuOpen = false;
+
+burger.addEventListener('click', () => {
+  if (!isMenuOpen) {
+    openMenu();
+  } else {
+    closeMenu();
+  }
+});
+
+function openMenu() {
+  menu.classList.add('open');
+  document.body.classList.add('menu-open');
+  isMenuOpen = true;
+}
+
+function closeMenu() {
+  menu.classList.remove('open');
+  document.body.classList.remove('menu-open');
+  isMenuOpen = false;
+}
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', () => {
+    closeMenu();
+  });
+});
